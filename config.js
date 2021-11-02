1445f8b83d26b9a9a21555a04a00188b8839e8be
minhduc2309 <93601146+minhduc2309@users.noreply.github.com>
2021-11-02 22:10:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'hi bbi nmkt!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'iu em s1',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnYes: 'iu anh ',
+    btnNo: 'không iu ',
+    question:'tại sao lại iu anh vậy ?',
+    btnReply: 'Gửi cho mduc <3',
+    reply: 'bởi vì yêu là yêu thoiii <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em s1 😘😘',
+    messDesc: 'Tối mai 7h anh qua chở đi chém lộn nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.messenger.com/t/100036731584342'
 }
