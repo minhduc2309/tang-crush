e694df3ba152b9c00d4a038de0f94845b65f87cf
minhduc2309 <93601146+minhduc2309@users.noreply.github.com>
2021-11-02 22:12:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'iu thao s1',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnYes: 'iu mduccc <33',
+    btnNo: 'Không, chúng ta chỉ là bạn :3',
+    question:'tại sao em lại yêu anh vậy <3',
+    btnReply: 'Gửi cho mduc <3',
+    reply: 'đơn giản là yêu thoii <33333333',
+    mess: 'Anh biết mà 😘😘',
+    messDesc: 'Tối mai 7h anh qua chở đi đánh lộn nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/ngduc06/'
+    messLink: 'https://www.messenger.com/t/100036731584342'
 }
